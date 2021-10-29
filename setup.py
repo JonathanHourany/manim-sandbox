5eabe238b7632bfe31710d423ecc0ebc095a3ded
Jonathan Hourany <jonhourany@gmail.com>
2021-10-28 22:03:49 -0700

    Init commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""A shim to allow Poetry to install packages in editable mode."""
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
---
+++ tutorials/quickstart/basic_scene2.py
@@ -0,0 +1,14 @@
+from manim import Scene, Square, Circle, Create, Transform, FadeOut, PINK, PI
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()
+        square.rotate(PI / 4)
+
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
